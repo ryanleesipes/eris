c9c09de779f85f916fe755e6bf0c8d83d4171b38
Ryan Sipes <ryan@sipes.us>
2022-11-14 10:16:02 -0700

    Update background.js
    ThemeIcons defined for light and dark Thunderbird theme.

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,22 @@
 let properties = {
     "title": "Discord",
     "url": "https://discord.com/app",
+    themeIcons: [
+        {
+          dark: "images/discord-16.png",
+          light: "images/discord-light-16.png",
+          size: 16
+        },
+        {
+          dark: "images/discord-32.png",
+          light: "images/discord-light-32.png",
+          size: 32
+        },
+        {
+          dark: "images/discord-64.png",
+          light: "images/discord-light-64.png",
+          size: 64
+        }
+      ]
 }
 messenger.spacesToolbar.addButton("Discord", properties)
